ff8b69b8a9682fe8fa305514367cc3e01a854c04
yuwe <yuwe@microsoft.com>
2023-07-26 20:51:02 +0800

    support chat command

--- src/teamsBot.ts
+++ src/teamsBot.ts
@@ -1,9 +1,32 @@
-import { TeamsActivityHandler } from "botbuilder";
+import { OpenAIClient, AzureKeyCredential } from "@azure/openai";
+import { TeamsActivityHandler, TurnContext } from "botbuilder";
 
 // An empty teams activity handler.
 // You can add your customization code here to extend your bot logic if needed.
 export class TeamsBot extends TeamsActivityHandler {
   constructor() {
     super();
+    
+    const endpoint = "https://openaiserviceyu.openai.azure.com/" ;
+    const azureApiKey = "40194e7d8bfe4ed9b6c6dc75c61bc791" ;
+    const client = new OpenAIClient(endpoint, new AzureKeyCredential(azureApiKey));
+    const deploymentName = "text-davinci-003";
+
+    this.onMessage(async (context, next) => {
+      let txt = context.activity.text;
+      const removedMentionText = TurnContext.removeRecipientMention(context.activity);
+      if (removedMentionText) {
+        // Remove the line break
+        txt = removedMentionText.toLowerCase().replace(/\n|\r/g, "").trim();
+      }
+
+      const response = await client.getCompletions(deploymentName, [txt]);
+      for (const choice of response.choices) {
+        await context.sendActivity(choice.text);
+      }
+      
+      // By calling next() you ensure that the next BotHandler is run.
+      await next();
+    });
   }
 }
