1f202f829f9f8cc23464676824d4985c4097cf56
yuwe <yuwe@microsoft.com>
2023-07-27 20:56:29 +0800

    read open ai settings from config

--- src/internal/config.ts
+++ src/internal/config.ts
@@ -4,6 +4,9 @@ const config = {
   storageConnectionString: process.env.WEBSITE_CONTENTAZUREFILECONNECTIONSTRING,
   envName: process.env.TEAMSFX_ENV,
   teamsAppId: process.env.TEAMS_APP_ID,
+  openAiKey: process.env.OPENAI_KEY,
+  openAiEndpoint: process.env.OPENAI_ENDPOINT,
+  openAiDeploymentName: process.env.OPENAI_DEPLOYMENT_NAME,
 };
 
 export default config;
--- src/teamsBot.ts
+++ src/teamsBot.ts
@@ -2,6 +2,7 @@ import { OpenAIClient, AzureKeyCredential } from "@azure/openai";
 import { TeamsActivityHandler, TurnContext } from "botbuilder";
 import { QueryDateCommandHandler } from "./queryDateCommandHandler";
 import { QueryHolidaysCommandHandler } from "./queryHolidaysCommandHandler";
+import config from "./internal/config";
 
 // An empty teams activity handler.
 // You can add your customization code here to extend your bot logic if needed.
@@ -9,10 +10,7 @@ export class TeamsBot extends TeamsActivityHandler {
   constructor() {
     super();
     
-    const endpoint = "https://openaiserviceyu.openai.azure.com/" ;
-    const azureApiKey = "40194e7d8bfe4ed9b6c6dc75c61bc791" ;
-    const client = new OpenAIClient(endpoint, new AzureKeyCredential(azureApiKey));
-    const deploymentName = "text-davinci-003";
+    const client = new OpenAIClient(config.openAiEndpoint, new AzureKeyCredential(config.openAiKey));
 
     this.onMessage(async (context, next) => {
       let txt = context.activity.text;
@@ -23,7 +21,7 @@ export class TeamsBot extends TeamsActivityHandler {
         txt = removedMentionText.toLowerCase().replace(/\n|\r/g, "").trim();
       }
 
-      const response = await client.getCompletions(deploymentName, [txt]);
+      const response = await client.getCompletions(config.openAiDeploymentName, [txt]);
       for (const choice of response.choices) {
           await context.sendActivity(choice.text);
         }
